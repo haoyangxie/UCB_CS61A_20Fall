fe75fd81bbbf49b9d6efd89d8f8cd54a0546fe74
谢昊旸 <xiehaoyang@nat-wireless-guest-reg-153-50.bu.edu>
2022-05-31 16:56:41 -0400

    lab13 finished

--- lab/lab13/lab13.sql
+++ lab/lab13/lab13.sql
@@ -2,17 +2,30 @@
 
 
 CREATE TABLE average_prices AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT category, avg(MSRP) as average_price
+  FROM products
+  GROUP BY category;
 
 
 CREATE TABLE lowest_prices AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT store, item, min(price) as lowest_price
+  FROM inventory
+  GROUP BY item;
+  
+CREATE TABLE helper AS
+  SELECT name
+  FROM products
+  GROUP BY category
+  HAVING min(MSRP / rating);
 
 
 CREATE TABLE shopping_list AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT helper.name, store
+  FROM helper, lowest_prices
+  WHERE helper.name = lowest_prices.item;
 
 
 CREATE TABLE total_bandwidth AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+  SELECT sum(b.Mbs)
+  From shopping_list as a, stores as b
+  where a.store = b.store;
